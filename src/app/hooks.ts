9e9ebc6b60c48c4f0b950e050b7b51169ff7d04a
Шувалов Константин Павлович <suvalov_k_qschool@qsoft.ru>
2024-03-25 13:16:44 +0300

    lint: Отформатировал код

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -3,8 +3,10 @@
 // usage of typed hooks throughout the application.
 // We disable the ESLint rule here because this is the designated place
 // for importing and re-exporting the typed versions of hooks.
+
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 import { useDispatch, useSelector } from "react-redux"
+
 import type { AppDispatch, RootState } from "./store"
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { RouterProvider } from "react-router-dom"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
+import { RouterProvider } from "react-router-dom"
+
 import { router } from "./app/route"
+import { store } from "./app/store"
 import "./index.css"
 
 const container = document.getElementById("root")
