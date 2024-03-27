6fc27e73963317149476ee50af5185d307796847
Шувалов Константин Павлович <suvalov_k_qschool@qsoft.ru>
2024-03-27 17:27:37 +0300

    fix: Исправил типы

--- src/components/ArtCard/ArtCard.tsx
+++ src/components/ArtCard/ArtCard.tsx
@@ -2,7 +2,18 @@ import { Card } from "antd"
 
 import "./ArtCard.css"
 
-const ArtCard = ({ item }) => {
+interface Item {
+  id: number
+  title: string
+  culture: string
+  primaryimageurl: string
+}
+
+interface Props {
+  item: Item
+}
+
+const ArtCard = ({ item }: Props) => {
   return (
     <Card hoverable className="art-card" title={item.title}>
       <div className="art-card__content">
--- src/components/CardList/cardsApiSlice.ts
+++ src/components/CardList/cardsApiSlice.ts
@@ -1,12 +1,27 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
-const initialState = {
+import type { RootState } from "../../app/store"
+
+interface Card {
+  id: number
+  title: string
+  culture: string
+  primaryimageurl: string
+}
+
+interface CardsState {
+  cards: Card[]
+  status: "idle" | "loading" | "succeeded" | "failed"
+  error: string | null // Указываем, что error может быть строкой или null
+}
+
+const initialState: CardsState = {
   cards: [],
   status: "idle",
   error: null,
 }
 
-export const fetchData = async page => {
+export const fetchData = async (page: number) => {
   const response = await fetch(
     `https://api.harvardartmuseums.org/object?apikey=b32e07c4-2a7c-4f05-aea1-0be744328ef4&hasimage=1&classification=26&q=imagepermissionlevel:0&page=${page}`,
   )
@@ -31,12 +46,12 @@ export const cardsApiSlice = createSlice({
       })
       .addCase(fetchCards.rejected, (state, action) => {
         state.status = "failed"
-        state.error = action.error.message
+        state.error = action.error.message || "An error occurred"
       })
   },
 })
 
-export const selectAllCards = state => state.cards.cards
+export const selectAllCards = (state: RootState): Card[] => state.cards.cards
 
-export const selectCardById = (state, postId) =>
+export const selectCardById = (state: RootState, postId: number) =>
   state.cards.cards.find(post => post.id === postId)
